f3c75531c28023fe33b137c9de65c312fa9b32f4
sushilKumar1212 <132672516+sushilKumar1212@users.noreply.github.com>
2023-09-17 11:46:08 +0530

    pick image from gallery

--- lib/features/auth/screens/user_information_screen.dart
+++ lib/features/auth/screens/user_information_screen.dart
@@ -13,10 +13,26 @@ class _UserInfomationScreenState extends ConsumerState<UserInfomationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Center(
+      body: SafeArea(
+          child: Center(
         child: Column(
           children: [
-            
+            Stack(
+              children: [
+                const CircleAvatar(
+                  radius: 64,
+                  backgroundImage: NetworkImage(
+                      "https://img.freepik.com/premium-vector/account-icon-user-icon-vector-graphics_292645-552.jpg?w=2000"),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.add_a_photo_rounded)),
+                ),
+              ],
+            ),
           ],
         ),
       )),
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,5 +1,22 @@
-import 'package:whatsapp_clone/utils/imports.dart';
+import 'dart:io';
 
+import 'package:image_picker/image_picker.dart';
+import 'package:whatsapp_clone/utils/imports.dart';
+// Show Snackbar
 void showSnackBar({required BuildContext ctx, required String content}) {
   ScaffoldMessenger.of(ctx).showSnackBar(SnackBar(content: Text(content)));
+}
+// Get image from Gallery
+Future<File?> pickImageFromGallery(BuildContext context) async {
+  File? image;
+  try {
+    final pickImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickImage != null) {
+      image = File(pickImage.path);
+    }
+  } catch (e) {
+    showSnackBar(ctx: context, content: e.toString());
+  }
+  return image;
 }
