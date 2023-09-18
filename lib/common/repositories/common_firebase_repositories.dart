b405cd42f2386753bf6ad714e222c5266cd92bfe
sushil <baymax732001@gmail.com>
2023-09-18 22:27:20 +0530

    function for uploading file to firebase

--- lib/common/repositories/common_firebase_repositories.dart
+++ lib/common/repositories/common_firebase_repositories.dart
@@ -1,9 +1,13 @@
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:whatsapp_clone/utils/imports.dart';
 
 class CommonFirebaseStorageRepositories {
   final FirebaseStorage firebaseStorage;
   CommonFirebaseStorageRepositories({required this.firebaseStorage});
-  Future<String> storeFilesToFirebase() async {
-    UploadTask uploadTask = firebaseStorage.ref().child(path);
+  Future<String> storeFilesToFirebase(String ref, File file) async {
+    UploadTask uploadTask = firebaseStorage.ref().child(ref).putFile(file);
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
   }
 }
